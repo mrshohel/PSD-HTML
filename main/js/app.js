ccab9b117f69978930a3c932d2c3c8cf9f41c3a0
mrshohel <86289775+mrshohel@users.noreply.github.com>
2023-04-11 00:53:32 +0600

    Add files via upload

---
+++ main/js/app.js
@@ -0,0 +1,30 @@
+
+
+$(document).ready(function(){
+
+
+    //STICKY MENU
+
+$(".js--section").waypoint(function(direction){
+    if (direction == "down"){
+        $("nav").addClass("sticky");
+    }
+    else{
+        $("nav").removeClass("sticky");
+    }
+});
+
+
+
+
+
+    // MIXITUP (PROTFOLIO SECTION)
+    var mixer = mixitup('.container');
+});
+
+function openNav(){
+    document.getElementById("myNav").style.width = "100%";
+}
+function closeNav(){
+    document.getElementById("myNav").style.width = "0%";
+}
